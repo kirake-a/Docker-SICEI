ee3bed869cabf3dfc300f5292964fde255c4af78
kirake <ruben_aalvarado@outlook.com>
2024-03-10 21:39:06 -0600

    bug fixed: @put and @delete working correctly

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,10 @@
 FROM python:3.11-slim
 
+# El directorio de trabajo es desde donde se ejecuta el contenedor al iniciarse
 WORKDIR /app
 
+# Copiamos todos los archivos del build context al directorio /app del contenedor
 COPY . /app
 
+# Ejecutamos pip para instalar las dependencias en el contenedor
 RUN pip install -r requirements.txt
--- main.py
+++ main.py
@@ -35,8 +35,8 @@ def edit_student(id_student: int, student: Student):
     raise HTTPException(status_code=404, detail="Student not found")
 
 @app.delete("/students/{id_student}", response_model=Student)
-def delete_student(id_student: int,):
-    for index, student in enumerate(student):
+def delete_student(id_student: int):
+    for index, student in enumerate(students):
         if student.id == id_student:
             del students[index]
             return student

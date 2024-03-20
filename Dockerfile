2c920d92ae58d444d527511a7c033485bd11b2eb
kirake <ruben_aalvarado@outlook.com>
2024-03-20 17:52:49 -0600

    Dockerfile changed

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,18 @@
-FROM python:3.11-slim
+FROM python:3.11
 
-# El directorio de trabajo es desde donde se ejecuta el contenedor al iniciarse
-WORKDIR /app
+# 
+COPY ./requirements.txt .
 
-# Copiamos todos los archivos del build context al directorio /app del contenedor
-COPY . /app
+# 
+RUN pip install --no-cache-dir --upgrade -r requirements.txt
 
-# Ejecutamos pip para instalar las dependencias en el contenedor
-RUN pip install -r requirements.txt
+ENV MY_VARIABLE_1 "FastAPI"
+ENV MY_VARIABLE_2 "Dockerrr"
+
+# 
+COPY . .
+
+EXPOSE 8000
+
+# 
+CMD ["uvicorn", "main:app"]
--- main.py
+++ main.py
@@ -12,16 +12,16 @@ def root():
     return {"details": f"Hello, World with {env['MY_VARIABLE_1']} and {env['MY_VARIABLE_2']}"}
 
 @app.get("/students/", response_model=List[Student])
-def get_students():
+async def get_students():
     return students;
 
 @app.post("/students/", response_model=Student)
-def new_student(student: Student):
+async def new_student(student: Student):
     students.append(student)
     return student
 
 @app.put("/students/{id_student}", response_model=Student)
-def edit_student(id_student: int, student: Student):
+async def edit_student(id_student: int, student: Student):
     for index in range(len(students)):
         if id_student == students[index].id:
             students[index] = student
@@ -29,7 +29,7 @@ def edit_student(id_student: int, student: Student):
     raise HTTPException(status_code=404, detail="Student not found")
 
 @app.delete("/students/{id_student}", response_model=Student)
-def delete_student(id_student: int):
+async def delete_student(id_student: int):
     for index, student in enumerate(students):
         if student.id == id_student:
             del students[index]
